5eb2ffb23262a4f5cb26b90ab388c32812104747
edsanchez1987 <59986189+edsanchez1987@users.noreply.github.com>
2020-07-12 17:26:19 -0700

    First Commit

---
+++ RecipeProject/RecipeProject/SeafoodLink.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecipeProject
+{
+    public partial class SeafoodLink : Form
+    {
+        public SeafoodLink()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PorkLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+    }
+}
